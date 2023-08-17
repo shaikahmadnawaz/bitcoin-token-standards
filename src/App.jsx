ff7d2832135150f08d0a9cf1ca97d01026f1352b
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-08-17 11:38:28 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-function App() {
-  
+import { BrowserRouter } from "react-router-dom";
+import Landing from "./pages/home";
 
+function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Landing />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
